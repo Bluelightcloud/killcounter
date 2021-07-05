69d59c413e96c492cb892d3df1fda30700451cd9
blcloud <nekurina@gmail.com>
2021-07-05 14:38:44 +0900

    prototype

---
+++ Form1.Designer.vb
@@ -0,0 +1,390 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.plusBtn = New System.Windows.Forms.Button()
+        Me.minusBtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.unchangingBox1 = New System.Windows.Forms.TextBox()
+        Me.unchangingBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.unchangingBox3 = New System.Windows.Forms.TextBox()
+        Me.unchangingBox4 = New System.Windows.Forms.TextBox()
+        Me.unchangingBox5 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SaveItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoadItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TweetItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TrackerItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.matchBox1 = New System.Windows.Forms.TextBox()
+        Me.victoryBox1 = New System.Windows.Forms.TextBox()
+        Me.unchangingBox6 = New System.Windows.Forms.TextBox()
+        Me.kideBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.dethBox1 = New System.Windows.Forms.TextBox()
+        Me.unchangingBox7 = New System.Windows.Forms.TextBox()
+        Me.plusBtn2 = New System.Windows.Forms.Button()
+        Me.addBtn = New System.Windows.Forms.Button()
+        Me.minusBtn2 = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'plusBtn
+        '
+        Me.plusBtn.Font = New System.Drawing.Font("Yu Gothic UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.plusBtn.Location = New System.Drawing.Point(278, 216)
+        Me.plusBtn.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.plusBtn.Name = "plusBtn"
+        Me.plusBtn.Size = New System.Drawing.Size(126, 55)
+        Me.plusBtn.TabIndex = 0
+        Me.plusBtn.Text = "+1"
+        Me.plusBtn.UseVisualStyleBackColor = True
+        '
+        'minusBtn
+        '
+        Me.minusBtn.Font = New System.Drawing.Font("Yu Gothic UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.minusBtn.Location = New System.Drawing.Point(13, 216)
+        Me.minusBtn.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.minusBtn.Name = "minusBtn"
+        Me.minusBtn.Size = New System.Drawing.Size(126, 55)
+        Me.minusBtn.TabIndex = 1
+        Me.minusBtn.Text = "-1"
+        Me.minusBtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Yu Gothic UI", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.Location = New System.Drawing.Point(144, 69)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(129, 96)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "0"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'unchangingBox1
+        '
+        Me.unchangingBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.unchangingBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.unchangingBox1.Font = New System.Drawing.Font("Yu Gothic UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.unchangingBox1.Location = New System.Drawing.Point(77, 138)
+        Me.unchangingBox1.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.unchangingBox1.Name = "unchangingBox1"
+        Me.unchangingBox1.ReadOnly = True
+        Me.unchangingBox1.Size = New System.Drawing.Size(62, 27)
+        Me.unchangingBox1.TabIndex = 4
+        Me.unchangingBox1.Text = "合計"
+        Me.unchangingBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'unchangingBox2
+        '
+        Me.unchangingBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.unchangingBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.unchangingBox2.Font = New System.Drawing.Font("Yu Gothic UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.unchangingBox2.Location = New System.Drawing.Point(276, 138)
+        Me.unchangingBox2.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.unchangingBox2.Name = "unchangingBox2"
+        Me.unchangingBox2.ReadOnly = True
+        Me.unchangingBox2.Size = New System.Drawing.Size(58, 27)
+        Me.unchangingBox2.TabIndex = 5
+        Me.unchangingBox2.Text = "kill"
+        Me.unchangingBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Yu Gothic UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox2.Location = New System.Drawing.Point(179, 229)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(48, 34)
+        Me.TextBox2.TabIndex = 6
+        Me.TextBox2.Text = "0"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'unchangingBox3
+        '
+        Me.unchangingBox3.BackColor = System.Drawing.SystemColors.Control
+        Me.unchangingBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.unchangingBox3.Font = New System.Drawing.Font("Yu Gothic UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.unchangingBox3.Location = New System.Drawing.Point(228, 241)
+        Me.unchangingBox3.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.unchangingBox3.Name = "unchangingBox3"
+        Me.unchangingBox3.ReadOnly = True
+        Me.unchangingBox3.Size = New System.Drawing.Size(40, 22)
+        Me.unchangingBox3.TabIndex = 7
+        Me.unchangingBox3.Text = "kill"
+        Me.unchangingBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'unchangingBox4
+        '
+        Me.unchangingBox4.BackColor = System.Drawing.SystemColors.Control
+        Me.unchangingBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.unchangingBox4.Font = New System.Drawing.Font("Yu Gothic UI", 25.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.unchangingBox4.Location = New System.Drawing.Point(179, 180)
+        Me.unchangingBox4.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.unchangingBox4.Name = "unchangingBox4"
+        Me.unchangingBox4.ReadOnly = True
+        Me.unchangingBox4.Size = New System.Drawing.Size(48, 45)
+        Me.unchangingBox4.TabIndex = 8
+        Me.unchangingBox4.Text = "↑"
+        Me.unchangingBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'unchangingBox5
+        '
+        Me.unchangingBox5.BackColor = System.Drawing.SystemColors.Control
+        Me.unchangingBox5.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.unchangingBox5.Font = New System.Drawing.Font("Yu Gothic UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.unchangingBox5.Location = New System.Drawing.Point(249, 38)
+        Me.unchangingBox5.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.unchangingBox5.Name = "unchangingBox5"
+        Me.unchangingBox5.ReadOnly = True
+        Me.unchangingBox5.Size = New System.Drawing.Size(55, 16)
+        Me.unchangingBox5.TabIndex = 10
+        Me.unchangingBox5.Text = "マッチ数 : "
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.MenuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveItem1, Me.LoadItem1, Me.TweetItem1, Me.TrackerItem1})
+        Me.MenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(417, 24)
+        Me.MenuStrip1.TabIndex = 11
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SaveItem1
+        '
+        Me.SaveItem1.Name = "SaveItem1"
+        Me.SaveItem1.Size = New System.Drawing.Size(60, 20)
+        Me.SaveItem1.Text = "セーブ(&S)"
+        '
+        'LoadItem1
+        '
+        Me.LoadItem1.Name = "LoadItem1"
+        Me.LoadItem1.Size = New System.Drawing.Size(58, 20)
+        Me.LoadItem1.Text = "ロード(&L)"
+        '
+        'TweetItem1
+        '
+        Me.TweetItem1.Name = "TweetItem1"
+        Me.TweetItem1.Size = New System.Drawing.Size(72, 20)
+        Me.TweetItem1.Text = "ツイート(&W)"
+        '
+        'TrackerItem1
+        '
+        Me.TrackerItem1.Name = "TrackerItem1"
+        Me.TrackerItem1.Size = New System.Drawing.Size(74, 20)
+        Me.TrackerItem1.Text = "トラッカー(&T)"
+        '
+        'matchBox1
+        '
+        Me.matchBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.matchBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.matchBox1.Font = New System.Drawing.Font("Yu Gothic UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.matchBox1.Location = New System.Drawing.Point(294, 38)
+        Me.matchBox1.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.matchBox1.Name = "matchBox1"
+        Me.matchBox1.Size = New System.Drawing.Size(26, 16)
+        Me.matchBox1.TabIndex = 12
+        Me.matchBox1.Text = "0"
+        Me.matchBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'victoryBox1
+        '
+        Me.victoryBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.victoryBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.victoryBox1.Font = New System.Drawing.Font("Yu Gothic UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.victoryBox1.Location = New System.Drawing.Point(216, 38)
+        Me.victoryBox1.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.victoryBox1.Name = "victoryBox1"
+        Me.victoryBox1.Size = New System.Drawing.Size(26, 16)
+        Me.victoryBox1.TabIndex = 14
+        Me.victoryBox1.Text = "0"
+        Me.victoryBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'unchangingBox6
+        '
+        Me.unchangingBox6.BackColor = System.Drawing.SystemColors.Control
+        Me.unchangingBox6.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.unchangingBox6.Font = New System.Drawing.Font("Yu Gothic UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.unchangingBox6.Location = New System.Drawing.Point(172, 38)
+        Me.unchangingBox6.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.unchangingBox6.Name = "unchangingBox6"
+        Me.unchangingBox6.ReadOnly = True
+        Me.unchangingBox6.Size = New System.Drawing.Size(55, 16)
+        Me.unchangingBox6.TabIndex = 13
+        Me.unchangingBox6.Text = "勝利数 : "
+        '
+        'kideBox1
+        '
+        Me.kideBox1.BackColor = System.Drawing.SystemColors.Control
+        Me.kideBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.kideBox1.Font = New System.Drawing.Font("Yu Gothic UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.kideBox1.Location = New System.Drawing.Point(128, 38)
+        Me.kideBox1.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.kideBox1.Name = "kideBox1"
+        Me.kideBox1.Size = New System.Drawing.Size(38, 16)
+        Me.kideBox1.TabIndex = 17
+        Me.kideBox1.Text = "0"
+        Me.kideBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox4.Font = New System.Drawing.Font("Yu Gothic UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox4.Location = New System.Drawing.Point(99, 38)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(55, 16)
+        Me.TextBox4.TabIndex = 16
+        Me.TextBox4.Text = "K/D : "
+        '
+        'dethBox1
+        '
+        Me.dethBox1.Font = New System.Drawing.Font("Yu Gothic UI", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.dethBox1.Location = New System.Drawing.Point(179, 302)
+        Me.dethBox1.Name = "dethBox1"
+        Me.dethBox1.Size = New System.Drawing.Size(48, 34)
+        Me.dethBox1.TabIndex = 18
+        Me.dethBox1.Text = "0"
+        Me.dethBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'unchangingBox7
+        '
+        Me.unchangingBox7.BackColor = System.Drawing.SystemColors.Control
+        Me.unchangingBox7.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.unchangingBox7.Font = New System.Drawing.Font("Yu Gothic UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.unchangingBox7.Location = New System.Drawing.Point(234, 314)
+        Me.unchangingBox7.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.unchangingBox7.Name = "unchangingBox7"
+        Me.unchangingBox7.ReadOnly = True
+        Me.unchangingBox7.Size = New System.Drawing.Size(39, 22)
+        Me.unchangingBox7.TabIndex = 19
+        Me.unchangingBox7.Text = "death"
+        Me.unchangingBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'plusBtn2
+        '
+        Me.plusBtn2.Font = New System.Drawing.Font("Yu Gothic UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.plusBtn2.Location = New System.Drawing.Point(278, 289)
+        Me.plusBtn2.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.plusBtn2.Name = "plusBtn2"
+        Me.plusBtn2.Size = New System.Drawing.Size(126, 55)
+        Me.plusBtn2.TabIndex = 20
+        Me.plusBtn2.Text = "+1"
+        Me.plusBtn2.UseVisualStyleBackColor = True
+        '
+        'addBtn
+        '
+        Me.addBtn.Cursor = System.Windows.Forms.Cursors.Default
+        Me.addBtn.Font = New System.Drawing.Font("Yu Gothic UI", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.addBtn.ForeColor = System.Drawing.Color.Black
+        Me.addBtn.Location = New System.Drawing.Point(156, 373)
+        Me.addBtn.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.addBtn.Name = "addBtn"
+        Me.addBtn.Size = New System.Drawing.Size(100, 66)
+        Me.addBtn.TabIndex = 3
+        Me.addBtn.Text = "加算"
+        Me.addBtn.UseVisualStyleBackColor = True
+        '
+        'minusBtn2
+        '
+        Me.minusBtn2.Font = New System.Drawing.Font("Yu Gothic UI", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.minusBtn2.Location = New System.Drawing.Point(13, 289)
+        Me.minusBtn2.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.minusBtn2.Name = "minusBtn2"
+        Me.minusBtn2.Size = New System.Drawing.Size(126, 55)
+        Me.minusBtn2.TabIndex = 21
+        Me.minusBtn2.Text = "-1"
+        Me.minusBtn2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(417, 450)
+        Me.Controls.Add(Me.minusBtn2)
+        Me.Controls.Add(Me.plusBtn2)
+        Me.Controls.Add(Me.unchangingBox7)
+        Me.Controls.Add(Me.dethBox1)
+        Me.Controls.Add(Me.kideBox1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.victoryBox1)
+        Me.Controls.Add(Me.unchangingBox6)
+        Me.Controls.Add(Me.matchBox1)
+        Me.Controls.Add(Me.unchangingBox5)
+        Me.Controls.Add(Me.unchangingBox4)
+        Me.Controls.Add(Me.unchangingBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.unchangingBox2)
+        Me.Controls.Add(Me.unchangingBox1)
+        Me.Controls.Add(Me.addBtn)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.minusBtn)
+        Me.Controls.Add(Me.plusBtn)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "キルカウンター"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents plusBtn As Button
+    Friend WithEvents minusBtn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents unchangingBox1 As TextBox
+    Friend WithEvents unchangingBox2 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents unchangingBox3 As TextBox
+    Friend WithEvents unchangingBox4 As TextBox
+    Friend WithEvents unchangingBox5 As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents matchBox1 As TextBox
+    Friend WithEvents victoryBox1 As TextBox
+    Friend WithEvents unchangingBox6 As TextBox
+    Friend WithEvents kideBox1 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents SaveItem1 As ToolStripMenuItem
+    Friend WithEvents TweetItem1 As ToolStripMenuItem
+    Friend WithEvents dethBox1 As TextBox
+    Friend WithEvents unchangingBox7 As TextBox
+    Friend WithEvents plus As Button
+    Friend WithEvents addBtn As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents plusBtn2 As Button
+    Friend WithEvents minusBtn2 As Button
+    Friend WithEvents LoadItem1 As ToolStripMenuItem
+    Friend WithEvents TrackerItem1 As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,189 @@
+﻿Imports System.IO
+Imports System.Text
+Imports MSScriptControl
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Public Class SaveClass
+        Public killsum As Integer
+        Public kd As Single
+        Public match As Integer
+        Public vic As Integer
+        Public dethsum As Integer
+    End Class
+
+    Dim num As Integer
+    Dim sum As Integer
+    Dim kd As Single
+    Dim kdst As String
+    Dim dethsum As Integer
+    Dim deth As Integer
+    Dim vic As Integer
+    Dim match As Integer
+
+    Private Sub plusBtn_Click(sender As Object, e As EventArgs) Handles plusBtn.Click
+        num = CInt(Fix(TextBox2.Text))
+        num += 1
+        TextBox2.Text = num
+
+    End Sub
+
+    Private Sub minusBtn_Click(sender As Object, e As EventArgs) Handles minusBtn.Click
+        num = CInt(Fix(TextBox2.Text))
+        If num > 0 Then
+            num -= 1
+        End If
+        TextBox2.Text = num
+
+    End Sub
+
+    Private Sub addBtn_Click(sender As Object, e As EventArgs) Handles addBtn.Click
+        num = CInt(Fix(TextBox2.Text))
+        sum = CInt(Fix(TextBox1.Text))
+        match = CInt(Fix(matchBox1.Text))
+        vic = CInt(Fix(victoryBox1.Text))
+        deth = CInt(Fix(dethBox1.Text))
+
+        If deth = 0 Then
+            vic += 1
+        End If
+
+        sum += num
+        match += 1
+        dethsum += deth
+
+        If dethsum = 0 Then
+            kd = sum
+        Else
+            kd = sum / dethsum
+        End If
+
+        kdst = kd.ToString("0.00")
+        TextBox1.Text = sum
+        matchBox1.Text = match
+        victoryBox1.Text = vic
+        kideBox1.Text = kdst
+    End Sub
+
+    Private Sub plusBtn2_Click(sender As Object, e As EventArgs) Handles plusBtn2.Click
+        deth = CInt(Fix(dethBox1.Text))
+        deth += 1
+        dethBox1.Text = deth
+
+    End Sub
+
+    Private Sub minusBtn2_Click(sender As Object, e As EventArgs) Handles minusBtn2.Click
+        deth = CInt(Fix(dethBox1.Text))
+        If deth > 0 Then
+            deth -= 1
+        End If
+        dethBox1.Text = deth
+
+    End Sub
+
+    Private Sub SaveItem1_Click(sender As Object, e As EventArgs) Handles SaveItem1.Click
+        Dim Ret As DialogResult
+        Dim FilePath As String = String.Empty
+        Dim obj As New SaveClass()
+        obj.killsum = sum
+        obj.kd = kdst
+        obj.dethsum = dethsum
+        obj.match = match
+        obj.vic = vic
+
+        Try
+
+            Using Dialog As New SaveFileDialog()
+
+                With Dialog
+                    .FileName = "save.xml"
+                    .Title = "名前を付けて保存"
+                    .Filter = "XMLファイル(*.xml)|*.xml|すべてのファイル(*.*)|*.*"
+                    .FilterIndex = 2
+                    .RestoreDirectory = True
+                End With
+
+                Ret = Dialog.ShowDialog()
+
+                If Ret = DialogResult.OK Then
+
+                    FilePath = Dialog.FileName
+                    Dim serializer As New System.Xml.Serialization.XmlSerializer(
+                        GetType(SaveClass))
+                    Using sw As New StreamWriter(FilePath, False, New System.Text.UTF8Encoding(False))
+
+                        serializer.Serialize(sw, obj)
+
+                    End Using
+
+                End If
+
+            End Using
+
+        Catch ex As Exception
+
+            MessageBox.Show(Err.Description,
+                "エラー",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error)
+
+        End Try
+    End Sub
+
+    Private Sub LoadItem1_Click(sender As Object, e As EventArgs) Handles LoadItem1.Click
+        Dim Ret As DialogResult
+        Dim FilePath As String = String.Empty
+        Dim SelectFile As String = String.Empty
+
+        Try
+
+            Using Dialog As New OpenFileDialog()
+
+                With Dialog
+                    .FileName = "save.xml"
+                    .Title = "読み込むデータを選択"
+                    .Filter = "XMLファイル(*.xml)|*.xml|すべてのファイル(*.*)|*.*"
+                    .FilterIndex = 2
+                    .RestoreDirectory = True
+                End With
+
+                Ret = Dialog.ShowDialog()
+
+                If Ret = DialogResult.OK Then
+
+                    FilePath = Dialog.FileName
+                    Dim serializer As New System.Xml.Serialization.XmlSerializer(
+                                           GetType(SaveClass))
+                    Using sr As New StreamReader(FilePath, New System.Text.UTF8Encoding(False))
+                        Dim obj As SaveClass =
+                        DirectCast(serializer.Deserialize(sr), SaveClass)
+                        TextBox1.Text = obj.killsum
+                        kideBox1.Text = obj.kd
+                        dethsum = obj.dethsum
+                        matchBox1.Text = obj.match
+                        victoryBox1.Text = obj.vic
+
+                    End Using
+                End If
+
+            End Using
+
+        Catch ex As Exception
+
+            MessageBox.Show(Err.Description,
+                "エラー",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error)
+
+        End Try
+    End Sub
+
+    Private Sub TweetItem1_Click(sender As Object, e As EventArgs) Handles TweetItem1.Click
+
+        Dim wsh = CreateObject("WScript.Shell")
+        Dim ie = CreateObject("InternetExplorer.Application")
+        ie.Visible = True
+
+    End Sub
+End Class
+
